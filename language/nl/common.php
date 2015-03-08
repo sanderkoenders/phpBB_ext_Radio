6ecf88cf1d1e2e4fc5d6b9983a5635025b86d3a8
Archcry <sanderkoenders@hotmail.com>
2015-03-08 11:39:21 +0100

    Dutch Language pack
    Translation for dutch.

---
+++ language/nl/common.php
@@ -0,0 +1,47 @@
+<?php
+/**
+*
+* @package phpBB Extension - Archcry Radio
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	// Main page
+	'STATION'					=> 'Huidige DJ',
+	'SONG'						=> 'Huidig Nummer',
+	'GENRE'						=> 'Genre',
+	'BITRATE'					=> 'Bitrate',
+	
+	// Json
+	'RADIO_NOT_AVAILABLE' 		=> 'Niet beschikbaar',
+	
+	// ACP languages variables
+	'ACP_RADIO_GENERAL'			=> 'Algemene Instellingen',
+	'ACP_RADIO_TITLE'			=> 'Radio Module',
+	'ACP_RADIO'					=> 'Instellingen',
+	'ACP_RADIO_HOST'			=> 'SHOUTcast Host',
+	'ACP_RADIO_USERAGENT'		=> 'Useragent',
+	'ACP_RADIO_PORT'			=> 'Poort',
+	'ACP_RADIO_USER'			=> 'Gebruikersnaam',
+	'ACP_RADIO_PASSWD'			=> 'Wachtwoord',
+	'ACP_RADIO_SETTING_SAVED'	=> 'SHOUTcast Instellingen Opgeslagen',
+	
+	// Links to files for music players
+	'ACP_RADIO_MUSIC_PLAYERS'	=> 'Links Voor Muziek Spelers',
+	'ACP_RADIO_WINAMP_URL'		=> 'Link Voor Winamp',
+	'ACP_RADIO_WMP_URL'			=> 'Link Voor Windows Media Player',
+	'ACP_RADIO_REAL_URL'		=> 'Link Voor RealPlayer',
+	'ACP_RADIO_ITUNES_URL'		=> 'Link Voor Itunes'
+));
