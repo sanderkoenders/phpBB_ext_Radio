3d0d0626fb5807855692fd0d0d160025dc34743c
Sander Koenders <sanderkoenders@gmail.com>
2016-05-20 20:00:34 +0200

    icecast support

---
+++ library/IcecastAdapter.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace archcry\radio\library;
+
+class IcecastAdapter extends Radio
+{
+    public function getInformation()
+    {
+        $radioInfo = $this->getInfoFromCache();
+
+        if ($radioInfo === false) {
+            $curl = curl_exec($this->initCurl($this->config));
+
+            if ($curl)
+            {
+                $json = @json_decode($curl);
+
+                $radioInfo = $this->formatData($json->icestats->source);
+            }
+            else
+            {
+                $radioInfo = array('error' => $this->user->lang('RADIO_NOT_AVAILABLE'));
+            }
+
+            // Save this to the cache to improve performance
+            $this->saveInfoToCache($radioInfo);
+        }
+
+        return $radioInfo;
+    }
+
+    /**
+     * Function to initialize a curl connection to the broadcast server
+     *
+     * @param $config
+     * @return resource
+     */
+    protected function initCurl($config)
+    {
+        // initialize curl connection
+        $ch = curl_init($config['archcry_radio_host'] . ':' . $config['archcry_radio_port'] . '/status-json.xsl');
+
+        // set curl connection parameter
+        curl_setopt($ch, CURLOPT_PORT, $config['archcry_radio_port']);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+
+        return $ch;
+    }
+
+    /**
+     * Function to format information received from the broadcast server
+     *
+     * @param $json
+     * @return array
+     */
+    protected function formatData($json)
+    {
+        $data = array(
+            'currentListeners' => (string)(!empty($json->listeners) ? $json->listeners : $this->user->lang('RADIO_NOT_AVAILABLE')),
+            'peakListeners' => (string)(!empty($json->listener_peak) ? $json->listener_peak : $this->user->lang('RADIO_NOT_AVAILABLE')),
+            'maxListeners' => $this->user->lang('RADIO_NOT_AVAILABLE'),
+            'reportedListeners' => $this->user->lang('RADIO_NOT_AVAILABLE'),
+            'avarageTime' => $this->user->lang('RADIO_NOT_AVAILABLE'),
+            'serverGenre' => (string)(!empty($json->genre) ? $json->genre : $this->user->lang('RADIO_NOT_AVAILABLE')),
+            'serverUrl' => (string)(!empty($json->listenurl) ? $json->listenurl : $this->user->lang('RADIO_NOT_AVAILABLE')),
+            'serverTitle' => (string)(!empty($json->server_name) ? $json->server_name : $this->user->lang('RADIO_NOT_AVAILABLE')),
+            'songTitle' => (string)(!empty($json->title) ? $json->title : $this->user->lang('RADIO_NOT_AVAILABLE')),
+            'nextTitle' => $this->user->lang('RADIO_NOT_AVAILABLE'),
+            'songUrl' => $this->user->lang('RADIO_NOT_AVAILABLE'),
+            'irc' => $this->user->lang('RADIO_NOT_AVAILABLE'),
+            'icq' => $this->user->lang('RADIO_NOT_AVAILABLE'),
+            'aim' => $this->user->lang('RADIO_NOT_AVAILABLE'),
+            'streamStatus' => $this->user->lang('RADIO_NOT_AVAILABLE'),
+            'bitrate' => (string)(!empty($json->bitrate) ? $json->bitrate : $this->user->lang('RADIO_NOT_AVAILABLE')),
+            'content' => $this->user->lang('RADIO_NOT_AVAILABLE')
+        );
+
+        return $data;
+    }
+}
--- library/RadioFactory.php
+++ library/RadioFactory.php
@@ -5,11 +5,16 @@ namespace archcry\radio\library;
 class RadioFactory
 {
     const SHOUTCAST = 'shoutcast';
+    const ICECAST = 'icecast';
 
     static public function initRadio($radioType, $cache, $config, $user) {
         switch($radioType) {
             case self::SHOUTCAST:
                 return new ShoutcastAdapter($cache, $config, $user);
+            break;
+
+            case self::ICECAST:
+                return new IcecastAdapter($cache, $config, $user);
             break;
         }
     }
